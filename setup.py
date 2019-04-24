dbbc678ec9ba4bb16322143cad3d5df8138b59ee
Casper Welzel Andersen <casper.andersen@epfl.ch>
2019-04-24 17:31:24 +0200

    Update README with release notes and versioning overviews.
    Added workflows written by @yakutovicha to `.qm` folder for future reference.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiida-export_migration-testing",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     license="MIT Licence",
     author="The AiiDA team",
-    install_requires=["aiida-core>=1.0.0b2"])
+    install_requires=["aiida-core>=1.0.0"])
